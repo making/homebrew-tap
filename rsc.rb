1db17e215bfc3ca572b39f8864b74bcc00e595a7
Toshiaki Maki <tmaki@pivotal.io>
2021-01-08 12:03:13 +0900

    Bump to rsc 0.7.0

--- rsc.rb
+++ rsc.rb
@@ -1,16 +1,16 @@
 class Rsc < Formula
   desc "RSocket Client CLI (RSC)"
   homepage "https://github.com/making/rsc"
-  version "0.6.1"
+  version "0.7.0"
 
   depends_on :arch => :x86_64
 
   if OS.mac?
     url "https://github.com/making/rsc/releases/download/#{version}/rsc-x86_64-apple-darwin"
-    sha256 "b71de3d76b2befb4893e7072624c32836dd703253c8e92d7bea10a1664f50890"
+    sha256 "192948d8f518c249f67dd67b77f873286568b526d70fdb2cc10ec384b92b3726"
   elsif OS.linux?
     url "https://github.com/making/rsc/releases/download/#{version}/rsc-x86_64-pc-linux"
-    sha256 "08b95842d8c735fad53c38bf601bcf1f4dfab4194f1688eff898b95f1f06185f"
+    sha256 "9bf9e19077fa93e4774b892860cd3e3ca130441a20576a69b22d8a59243afcbb"
   end
 
   def install
