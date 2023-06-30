f29147f24328577d8beb5b3eb49d0abe3556549d
Toshiaki Maki <tmaki@vmware.com>
2023-06-30 22:34:35 +0900

    update

--- Formula/bfc.rb
+++ Formula/bfc.rb
@@ -8,10 +8,10 @@ class Bfc < Formula
 
   if OS.mac?
     url "https://github.com/making/bfc/releases/download/#{version}/bfc-x86_64-apple-darwin"
-    sha256 "f35f0d8393ecc53a29f18e02d60d98a9afaa2b680c9369184982b55e7d26c026"
+    sha256 "8553e7682060b942b3e1904f7a061f8391ac7ef0bea0e307cbdcdb04d7c984f5"
   elsif OS.linux?
     url "https://github.com/making/bfc/releases/download/#{version}/bfc-x86_64-pc-linux"
-    sha256 "c91e93dbfe445bd07072abee73fa6deebb2416ae2fe3340df5a0854905962e2b"
+    sha256 "6e8fd3dd152a51a6f10cfae1ba44af806ece59a44e0b45a1ecf612a0a736cb5a"
   end
 
   def install
